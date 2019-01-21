33a740016665ad4978ea15cc89c3110e15c50c6b
Stewart Palmer <stewart@stewartpalmer.com>
2019-01-21 10:02:38 -0500

    More experimental code

---
+++ exp/stuff.el
@@ -0,0 +1,42 @@
+
+
+
+
+;;
+;;  shu-add-to-alist
+;;
+(defmacro shu-add-to-alist (added-item new-item alist &optional testfn)
+  "Add an item to an alist.  The car of NEW-ITEM is a key to be added to the
+alist ALIST.  If the key does not already exist in ALIST, NEW-ITEM is added to
+ALIST.  ADDED-ITEM is either the item that was added or the item that was
+previously there.  If (eq ADDED-ITEM NEW-ITEM), then NEW-ITEM was added to the
+list.  If (not (eq ADDED-ITEM NEW-ITEM)), then the key already existed in the
+list and ADDED-ITEM is the item that was already on the list with a matching
+key."
+    `(if (not ,alist)
+        (setq ,alist (cons ,new-item ,alist))
+      (setq ,added-item (assoc (car ,new-item) ,alist ,testfn))
+      (when (not ,added-item)
+        (setq ,alist (cons ,new-item ,alist))
+        )
+      )
+    )
+
+
+;;
+;;  aaa
+;;
+(defun aaa ()
+  "Doc string."
+  (interactive)
+  (let (
+        (gb (get-buffer-create "**boo**"))
+        (x)
+        (added-item)
+        (new-item)
+        (alist)
+        )
+    (setq x (macroexpand '(shu-add-to-alist added-item new-item alist)))
+    (princ x gb)
+
+    ))
