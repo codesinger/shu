b14c46e014fde1e05c001f3322aba747c8051e2d
Stewart Palmer <stewart@stewartpalmer.com>
2019-01-21 10:27:51 -0500

    More experimental code

--- exp/stuff.el
+++ exp/stuff.el
@@ -3,6 +3,33 @@
 
 
 ;;
+;;  shu-add-to-alist-1
+;;
+(defmacro shu-add-to-alist-1 (added-item new-item alist &optional testfn)
+  "Add an item to an alist.  The car of NEW-ITEM is a key to be added to the
+alist ALIST.  If the key does not already exist in ALIST, NEW-ITEM is added to
+ALIST.  ADDED-ITEM is either the item that was added or the item that was
+previously there.  If (eq ADDED-ITEM NEW-ITEM), then NEW-ITEM was added to the
+list.  If (not (eq ADDED-ITEM NEW-ITEM)), then the key already existed in the
+list and ADDED-ITEM is the item that was already on the list with a matching
+key."
+  `(if (not ,alist)
+       (progn
+         (setq ,alist (cons ,new-item ,alist))
+         (setq ,added-item ,new-item)
+         )
+      (setq ,added-item (assoc (car ,new-item) ,alist ,testfn))
+      (when (not ,added-item)
+        (setq ,alist (cons ,new-item ,alist))
+        )
+      )
+    )
+
+
+
+
+
+;;
 ;;  shu-add-to-alist
 ;;
 (defmacro shu-add-to-alist (added-item new-item alist &optional testfn)
@@ -13,11 +40,14 @@ previously there.  If (eq ADDED-ITEM NEW-ITEM), then NEW-ITEM was added to the
 list.  If (not (eq ADDED-ITEM NEW-ITEM)), then the key already existed in the
 list and ADDED-ITEM is the item that was already on the list with a matching
 key."
-    `(if (not ,alist)
-        (setq ,alist (cons ,new-item ,alist))
+  `(if (not ,alist)
+       (progn
+         (push new-item alist)
+         (setq ,added-item ,new-item)
+         )
       (setq ,added-item (assoc (car ,new-item) ,alist ,testfn))
       (when (not ,added-item)
-        (setq ,alist (cons ,new-item ,alist))
+        (push new-item alist)
         )
       )
     )
