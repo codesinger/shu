a9ed8250dec6b441c06e24ae138c15eb10518006
Stewart Palmer <stewart@stewartpalmer.com>
2019-01-27 06:22:48 -0500

    Modify test/run.el to run on older versions of emacs

--- test/run.el
+++ test/run.el
@@ -12,23 +12,24 @@
   (let
       ((test-files
         (list
-;;;         "find.t.el"
-;;;         "macros.t.el"
-;;;         "s-mode.t.el"
-         "shu-base.t.el"
-         "shu-capture-doc.t.el"
-         "shu-cpp-general.t.el"
-         "shu-cpp-project.t.el"
-         "shu-cpp-token.t.el"
-         "shu-misc.t.el"
-         "shu-nvplist.t.el"
-         "shu-xref.t.el"))
+;;;         "find.t.elc"
+;;;         "macros.t.elc"
+;;;         "s-mode.t.elc"
+         "shu-base.t.elc"
+         "shu-capture-doc.t.elc"
+         "shu-cpp-general.t.elc"
+         "shu-cpp-project.t.elc"
+         "shu-cpp-token.t.elc"
+         "shu-misc.t.elc"
+         "shu-nvplist.t.elc"
+         "shu-xref.t.elc"))
        (file-name)
        (tlist))
+    (byte-force-recompile ".")
     (setq tlist test-files)
     (while tlist
       (setq file-name (car tlist))
-      (byte-recompile-file file-name 0 0 1)
+      (load-file file-name)
       (setq tlist (cdr tlist)))
     (ert t)
     ))
