28eeadd160acd0842785ae7197fd37b2eb920814
Stewart Palmer <stewart@stewartpalmer.com>
2019-02-09 09:39:46 -0500

    Minor change to npglog, in personal/useful.el

--- personal/useful.el
+++ personal/useful.el
@@ -121,7 +121,8 @@ of every visited file.  Makes a useful history of all files visited."
      (concat
       "BALL_LOG_INFO_BLOCK\n"
       pad "{\n"
-      pad ipad "BALL_LOG_OUTPUT_STREAM << "))
+      pad ipad "BALL_LOG_OUTPUT_STREAM\n"
+      pad ipad "    << "))
     (setq pos (point))
     (insert
      (concat
