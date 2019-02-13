a5a460ebf12fcdddb4e1e4fe73346cced5130ec1
Stewart Palmer <stewart@stewartpalmer.com>
2019-02-13 09:01:38 -0500

    Update version number

--- lisp/shu-base.el
+++ lisp/shu-base.el
@@ -4,7 +4,7 @@
 ;;
 ;; Package: shu-base
 ;; Author: Stewart L. Palmer <stewart@stewartpalmer.com>
-;; Version: 1.4.7
+;; Version: 1.4.8
 ;; Homepage: https://github.com/codesinger/shu.git
 ;;
 ;; This file is NOT part of GNU Emacs.
@@ -35,7 +35,7 @@
 
 (provide 'shu-base)
 
-(defconst shu-version "1.4.7"
+(defconst shu-version "1.4.8"
   "The version number of the Shu elisp package.")
 
 (defconst shu-date "2019 Feb 2"
