05163780ca920af1cdd843a7009c69771324f89c
Stewart Palmer <stewart@stewartpalmer.com>
2019-02-19 07:36:04 -0500

    Update version number

--- lisp/shu-overview.el
+++ lisp/shu-overview.el
@@ -38,7 +38,7 @@
 ;;
 ;; Version 1.4.0 was merged with the master branch on 2 February 2019.
 ;;
-;; This is Version 1.4.9 of the Shu elisp repository,
+;; This is Version 1.4.10 of the Shu elisp repository,
 ;;
 ;; What this document lacks lacks are detailed scenarios and work flows.  The
 ;; reader might well say that this is an interesting collection of parts, and
