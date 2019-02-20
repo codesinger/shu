ead8a11284f55d4ed96085ead9f3cd1f6eff40dc
Stewart Palmer <stewart@stewartpalmer.com>
2019-02-20 06:28:11 -0500

    Start shu-cpp-match.el

---
+++ exp/shu-cpp-match.el
@@ -0,0 +1,343 @@
+;;; shu-cpp-match.el --- Shu project code for dealing wth C++ in Emacs
+;;
+;; Copyright (C) 2019 Stewart L. Palmer
+;;
+;; Package: shu-cpp-match
+;; Author: Stewart L. Palmer <stewart@stewartpalmer.com>
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; This is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This software is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; There is a copy of the Gnu General Public license in the file
+;; LICENSE in this repository.  You should also have received a copy
+;; of the GNU General Public License along with GNU Emacs.  If not,
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Functions to match patterns against list of tokens produced by
+;; shu-cpp-token.el.
+;; is shown below.
+
+;;; Code:
+
+(provide 'shu-cpp-match)
+(require 'shu-cpp-token)
+
+
+;;
+;;  match-list:
+;;
+;;   -------------------
+;;   |        |        |
+;;   |    o   |   o    |
+;;   |    |   |   |    |
+;;   -----|-------|-----
+;;        |       |
+;;        |       +-----> Next in list
+;;        |
+;;        +-------------> match-info
+;;
+;;
+;;
+;;
+;;  match-info
+;;
+;;   -------------------
+;;   |        |        |
+;;   |    o   |   o    |
+;;   |    |   |   |    |
+;;   -----|-------|-----
+;;        |       |
+;;        |       +-----> match-ext
+;;        |
+;;        +-------------> op-code
+;;
+;;
+;;
+;;
+;;  match-ext
+;;
+;;   -------------------
+;;   |        |        |
+;;   |    o   |   o    |
+;;   |    |   |   |    |
+;;   -----|-------|-----
+;;        |       |
+;;        |       +-----> match-type
+;;        |
+;;        +-------------> match-func
+;;
+;;
+;;
+;;
+;;  match-func
+;;
+;;   -------------------
+;;   |        |        |
+;;   |    o   |   o    |
+;;   |    |   |   |    |
+;;   -----|-------|-----
+;;        |       |
+;;        |       +-----> match-eval-func
+;;        |
+;;        +-------------> match-ret-ind
+;;
+;;
+;;
+;;
+;;  match-type
+;;
+;;   -------------------
+;;   |        |        |
+;;   |    o   |   o    |
+;;   |    |   |   |    |
+;;   -----|-------|-----
+;;        |       |
+;;        |       +-----> match-token-value
+;;        |
+;;        +-------------> match-token-type
+;;
+;;
+;;
+;; op-code saya what the matching operation is
+;; match-eval-func - If non-nil, call this function to do tbe evaluation
+;; match-ret-ind - If true, return this token-info as the return value
+;;                 iff the evaluation succeeds
+;; match-token-type - Is a token type to compare against the token-type from
+;;                    the token-info
+;; match-token-value - Is a possible value or regular expresson to be compared
+;;                     with or applied to the token-value from the token-info
+;;
+;;
+
+
+
+
+(defconst shu-cpp-token-match-type-skip 1
+  "The match type constant that indicates skip one input cell.")
+
+
+(defconst shu-cpp-token-match-type-same 2
+  "The match type constant that indicates that the token type and token value
+must both match.")
+
+
+(defconst shu-cpp-token-match-type-same-rx 3
+  "The match type constant that indicates that the token type must match and
+the token value must staisify the regular expression for a C++ variable name.")
+
+
+
+;;
+;;  shu-cpp-make-match-info
+;;
+(defun shu-cpp-make-match-info (match-info op-code match-eval-func
+                                match-ret-ind match-token-type match-token-value)
+  "Doc string."
+  (let (
+        (match-type)
+        (match-func)
+        (match-ext)
+        )
+    (setq match-type (cons match-token-type match-token-value))
+    (setq match-func (cons match-ret-ind match-eval-func))
+    (setq match-ext (cons match-func match-type))
+    (setq match-info (cons op-code match-ext))
+    ))
+
+
+
+;;
+;;  shu-cpp-match-extract-info
+;;
+(defmacro shu-cpp-match-extract-info (match-info op-code match-eval-func
+                                      match-ret-ind match-token-type match-token-value)
+  "Extract the information out of a match-info"
+  (let (
+        (tmatch-ext (make-symbol "match-ext"))
+        (tmatch-func (make-symbol "match-func"))
+        (tmatch-type (make-symbol "match-type"))
+        )
+    `(let (
+           (,tmatch-ext)
+           (,tmatch-func)
+           (,tmatch-type)
+           )
+       (setq ,op-code (car ,match-info))
+       (setq ,tmatch-ext (cdr ,match-info))
+       (setq ,tmatch-func (car ,tmatch-ext))
+       (setq ,tmatch-type (cdr ,tmatch-ext))
+       (setq ,match-eval-func (cdr ,tmatch-func))
+       (setq ,match-ret-ind (car ,tmatch-func))
+       (setq ,match-token-type (car ,tmatch-type))
+       (setq ,match-token-value (cdr ,tmatch-type))
+       )
+    ))
+
+
+;;
+;;  shu-cpp-match-extract-type
+;;
+(defsubst shu-cpp-match-extract-type (match-info)
+  "Doc string."
+  (interactive)
+  (let (
+        (match-ext)
+        (match-type)
+        )
+    (setq match-ext (cdr match-info))
+    (setq match-type (cdr match-ext))
+    (car match-type)
+    ))
+
+
+;;
+;;  shu-cpp-match-extract-token
+;;
+(defsubst shu-cpp-match-extract-token (match-info)
+  "Doc string."
+  (interactive)
+  (let (
+        (match-ext)
+        (match-type)
+        )
+    (setq match-ext (cdr match-info))
+    (setq match-type (cdr match-ext))
+    (cdr match-type)
+    ))
+
+
+
+;;
+;;  shu-cpp-match-tokens
+;;
+(defun shu-cpp-match-tokens (match-lists token-list)
+  "match-lists is a list of match lists.  token-list is a list of tokens.  for
+each match-list in match-lists, try to match every element of the match list to
+the token list.  if a match fails or if you rach the end of the token list
+before reaching the end of the match list, move to the next match list and try
+again.  if all elements of a match list match the tokens in the token list, stop
+the matching process and return non-nil.
+
+when matching of a match list succeeds, this is how the return value is
+determined.  if the return value indicator is true in one of the match-info of a
+successful match-list, return the token-info that matched, othertise return t."
+  (let (
+        (gb (get-buffer-create "**boo**"))
+        (match-list)
+        (match-info)
+        (outer-done)
+        (inner-done)
+        (did-match)
+        (mlist)
+        (tlist)
+        (token-info)
+        (return-value)
+        (op-code)
+        (match-eval-func)
+        (match-ret-ind)
+        (match-token-type)
+        (match-token-value)
+        )
+    (while (and match-lists (not outer-done))
+      (setq mlist (car match-lists))
+      (setq tlist token-list)
+      (setq inner-done nil)
+      (setq return-value nil)
+      (while (and tlist mlist (not inner-done))
+        (setq match-info (car mlist))
+        (setq token-info (car tlist))
+        (shu-cpp-match-extract-info match-info op-code match-eval-func
+                                    match-ret-ind match-token-type match-token-value)
+        (when  (/= op-code shu-cpp-token-match-type-skip)
+          (let (
+                (token)
+                )
+            (setq token (shu-cpp-token-extract-token token-info))
+            (princ (format "token from token-info: %s\n" token) gb)
+            (princ (format "match-token-value: %s\n" match-token-value) gb)
+            )
+          (setq did-match (funcall match-eval-func match-info token-info))
+          (if (not did-match)
+              (setq inner-done t)
+            (when match-ret-ind
+              (setq return-value token-info)
+              )
+            )
+          )
+        (setq mlist (cdr mlist))
+        (when tlist
+          (setq tlist (cdr tlist))
+          )
+        )
+      (when (not mlist)
+        (setq outer-done t)
+        (when (not return-value)
+          (setq return-value t)
+          )
+        )
+      (setq match-lists (cdr match-lists))
+      )
+    return-value
+    ))
+
+
+;;
+;;  shu-cpp-token-match-skip
+;;
+(defun shu-cpp-token-match-skip (tlist)
+  "Skip one cell in the input list."
+  (if tlist
+      (cdr tlist)
+    tlist)
+  )
+
+
+;;
+;;  shu-cpp-token-match-same
+;;
+(defun shu-cpp-token-match-same (match-info token-info)
+  "Doc string."
+  (interactive)
+  (let (
+        (gb (get-buffer-create "**boo**"))
+        (match-token-type (shu-cpp-match-extract-type match-info))
+        (match-token (shu-cpp-match-extract-token match-info))
+        (token-type (shu-cpp-token-extract-type token-info))
+        (token (shu-cpp-token-extract-token token-info))
+        )
+    (princ (format "shu-cpp-token-match-same: token-type: %d, match-token-type: %d, token: \"%s\", match-token: \"%s\"\n" token-type match-token-type token match-token) gb)
+    (and (= token-type match-token-type)
+         (string= token match-token))
+    ))
+
+
+;;
+;;  shu-cpp-token-match-same-rx
+;;
+(defun shu-cpp-token-match-same-rx (match-info token-info)
+  "Doc string."
+  (interactive)
+  (let (
+        (match-token-type (shu-cpp-match-extract-type match-info))
+        (token-type (shu-cpp-token-extract-type token-info))
+        (token (shu-cpp-token-extract-token token-info))
+        (rx (concat shu-cpp-name "+"))
+        )
+    (and (= token-type match-token-type)
+         (string-match rx token))
+
+    ))
+
+
+;;; shu-cpp-match.el ends here
---
+++ exp/shu-cpp-match.t.el
@@ -0,0 +1,35 @@
+;;; shu-cpp-match.t.el --- Shu project code for dealing wth C++ in Emacs
+;;
+;; Copyright (C) 2019 Stewart L. Palmer
+;;
+;; Package: shu-cpp-match
+;; Author: Stewart L. Palmer <stewart@stewartpalmer.com>
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;; This is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This software is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; There is a copy of the Gnu General Public license in the file
+;; LICENSE in this repository.  You should also have received a copy
+;; of the GNU General Public License along with GNU Emacs.  If not,
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Unit tests for the functions in shiu-match.el
+
+;;; Code:
+
+(provide 'shu-cpp-match)
+(require 'shu-cpp-token)
+
+
+;;; shu-cpp-match.t.el ends here
