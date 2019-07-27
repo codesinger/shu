ed188b60d1d51bfdfbba37b14054b8baf2de548f
Stewart Palmer <stewart@stewartpalmer.com>
2019-07-27 08:47:48 -0400

    More match unit tests

--- exp/shu-cpp-match.t.el
+++ exp/shu-cpp-match.t.el
@@ -1271,6 +1271,52 @@
 ))
 
 
+
+
+;;
+;;  shu-test-shu-cpp-match-repeat-list-7
+;;
+(ert-deftest shu-test-shu-cpp-match-repeat-list-7 ()
+  (let ((data
+         (concat
+          " :: :: bbbb"
+          "    cccc"
+          ))
+        (match-list
+         (list
+          (shu-cpp-make-match-info  shu-cpp-token-match-type-same
+                                    'shu-cpp-token-match-same
+                                    nil shu-cpp-token-type-op
+                                    "::")
+          (shu-cpp-make-match-info  shu-cpp-token-match-type-same-rx
+                                    'shu-cpp-token-match-same-rx
+                                    t shu-cpp-token-type-uq
+                                    (concat shu-cpp-name "+"))))
+        (rlist)
+        (token-list)
+        (match-info)
+        (ret-val)
+        (new-token-list)
+        (new-rlist)
+        (token)
+        (token-type)
+        (token-info))
+    (setq match-info (shu-cpp-make-match-side-list shu-cpp-token-match-type-side-loop
+                                                  match-list))
+    (with-temp-buffer
+      (insert data)
+      (setq token-list (shu-cpp-tokenize-region-for-command (point-min) (point-max))))
+    (setq ret-val (shu-cpp-match-repeat-list rlist token-list match-info))
+    (should ret-val)
+    (should (consp ret-val))
+    (setq new-token-list (car ret-val))
+    (setq new-rlist (cdr ret-val))
+    (should new-token-list)
+    (should (listp new-token-list))
+    (should (not new-rlist))
+    ))
+
+
 ;;
 ;;  shu-test-shu-cpp-extract-namespace-name
 ;;
