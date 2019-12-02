68ecdf2cc8cc426e1d8398ea04b68b25db0b81ef
Stewart Palmer <stewart@stewartpalmer.com>
2019-12-02 06:43:11 -0500

    More experimental code

--- exp/shu-new2.el
+++ exp/shu-new2.el
@@ -32,32 +32,34 @@
 
 
 ;;
-;;  csp
+;;  shu-csplit
 ;;
-(defun csp (prefix)
+(defun shu-csplit (prefix)
   "Doc string."
   (interactive "*P")
+  (shu-internal-csplit prefix)
+  )
+
+
+;;
+;;  shu-internal-csplit
+;;
+(defun shu-internal-csplit (&optional fixed-width)
+  "Doc string."
   (let (
-        (gb (get-buffer-create "**boo**"))
         (xquote "[^\\]\"") ;; quote not preceded by escape
         (tstart (shu-point-in-string))
         (tend)
-        (p)
-        (m)
         (cc)
         (pad-count 0)
         (bpad "")
         (npad "")
         (line-limit 10)
         (original)
-        (fixed-width prefix)
         (lines)
         (line)
         (nl "")
         )
-    ;; String to be removed is (tstart - 1) to tend (includes the quotes
-    ;; String to be split and re-inserted is from tstart to (tend - 1 (Not
-    ;; including the quotes
     (if (not tstart)
         (progn
           (ding)
@@ -76,15 +78,13 @@
           )
         )
       (setq line-limit (1- line-limit))
-      (princ (format "shu-cpp-line-end: %d, pad-count: %d, line-limit: %d\n" shu-cpp-line-end pad-count line-limit) gb)
       (setq tend (re-search-forward xquote nil t))
       (if (not tend)
-          (message "%s" "No string end")
-        (setq m (buffer-substring-no-properties tstart (1- tend)))
-        (setq m (concat "[" m "]"))
-        (message "%s" m)
+          (progn
+            (ding)
+            (message "%s" "No string end")
+            )
         (setq original (buffer-substring-no-properties tstart (1- tend)))
-        (princ (concat "[" original "]\n") gb)
         (setq lines (shu-misc-split-string original line-limit fixed-width))
         (goto-char (1- tstart))
         (delete-region (1- tstart) tend)
@@ -95,17 +95,301 @@
           (setq npad bpad)
           (setq lines (cdr lines))
           )
-
         )
       )
 
     ))
 
-                           ;;; "This is a string of some sort within these "
-                               "holy portals, revenge remains unknown and to "
-                               "all erring mortals, their way by love is shown "
-                               "and something "
-                               "else."
+                           ;;; "This is a string of some sort within these holy portals, revenge remains unknown and to all erring mortals, their way by love is shown and something else."
+
+
+
+
+
+;;
+;;  shu-misc-get-chunk
+;;
+(defun shu-misc-get-chunk (line-limit &optional escape)
+  "Return a string that consists of the first LINE-LIMIT characters in the
+current buffer.  If LINE-LIMIT is larger than the buffer size, return a
+string that is the entire contents of the buffer.  Before returning, delete
+from the buffer the returned string."
+  (let* (
+         (gb (get-buffer-create "**foo**"))
+         (spoint (point-min))
+         (xpoint (+ line-limit spoint))
+         (epoint (if (< (point-max) xpoint) (point-max) xpoint))
+         (last-char)
+         (part)
+         )
+    (princ (format "\nline-limit: %d, (point-max: %d, spoint: %d, epoint %d\n" line-limit (point-max) spoint epoint) gb)
+;;    (goto-char (1- epoint))
+    (when (and (> (point-max) 3) (> line-limit 3))
+      (setq last-char (buffer-substring-no-properties (1- epoint) epoint))
+      (when (and escape (string= last-char "\\"))
+        (setq epoint (1- epoint))
+        (setq last-char (buffer-substring-no-properties (1- epoint) epoint))
+        (when (string= last-char "\\")
+          (setq epoint (1- epoint))
+          )
+        )
+      )
+    (setq part (buffer-substring-no-properties spoint epoint))
+    (delete-region spoint epoint)
+    part
+    ))
+
+
+
+
+
+;;
+;;  shu-test-shu-misc-get-chunk-5
+;;
+(ert-deftest shu-test-shu-misc-get-chunk-5 ()
+  (let (
+        (gb (get-buffer-create "**foo**"))
+        (data "\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t")
+        (part)
+        (expected-part "\\t\\t\\t\\t\\t")
+        (expected-rest "")
+        (actual-rest)
+        (escape t)
+          )
+    (with-temp-buffer
+      (insert data)
+      (setq part (shu-misc-get-chunk 10 escape))
+      (should part)
+      (should (stringp part))
+      (princ (concat "\npart:\n [" part "]") gb)
+      (should (string= expected-part part))
+      )
+    ))
+
+
+
+
+;;
+;;  shu-test-shu-misc-get-chunk-6
+;;
+(ert-deftest shu-test-shu-misc-get-chunk-6 ()
+  (let (
+        (gb (get-buffer-create "**foo**"))
+        (data "\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t")
+        (part)
+        (expected-part "\\t\\t\\t\\t")
+        (expected-rest "")
+        (actual-rest)
+        (escape t)
+          )
+    (with-temp-buffer
+      (insert data)
+      (setq part (shu-misc-get-chunk 9 escape))
+      (should part)
+      (should (stringp part))
+      (princ (concat "\npart2:\n [" part "]") gb)
+      (should (string= expected-part part))
+      )
+    ))
+
+
+
+(defconst shu-test-cpp-general-base-string
+  (concat
+   "\"abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-={}[]|:;'<>,.?ABCDEFGHIJKLMNOPQ"
+   "RSTUVWXYZ0987654321!@#$%^&*()_+-={}[]|:;'<>,.?abcdefghijklmnopqrstuvwxyz12345678"
+   "90!@#$%^&*()_+-={}[]|:;'<>,.?ABCDEFGHIJKLMNOPQ\"")
+  ;; 12345678901234567890123456789012345678901234567890123456789012345678901234567890
+  ;;          1         2         3         4         5         6         7         8
+  "A test string for unit tests.")
+
+(defconst shu-test-cpp-general-expected-split1
+  (concat
+"\"abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-={}[]|:;'<>,.?ABCDEFGHIJKLMNO\"\n"
+"\"PQRSTUVWXYZ0987654321!@#$%^&*()_+-={}[]|:;'<>,.?abcdefghijklmnopqrstuvwxyz1234\"\n"
+"\"567890!@#$%^&*()_+-={}[]|:;'<>,.?ABCDEFGHIJKLMNOPQ\"")
+  "A test string for unit tests.")
+
+
+
+;;
+;;  shu-test-shu-csplit-1
+;;
+(ert-deftest shu-test-shu-csplit-1 ()
+  (let (
+        (gb (get-buffer-create "**foo**"))
+        (actual-split)
+        (actual-unsplit)
+        )
+    ;; Split of one long line starting in column 1
+    (setq shu-cpp-line-end 80)
+    (setq debug-on-error t)
+  (with-temp-buffer
+      (insert shu-test-cpp-general-base-string)
+      (goto-char (point-min))  ;; Sitting on top of open quote
+      (shu-internal-csplit)             ;; Try to split
+      (should (= 1 (point)))   ;; Nothing should have happened
+      (forward-char 1)         ;; Move to char right after quote
+      (shu-internal-csplit t)             ;; Now try to split
+      (should (= 215 (point))) ;; Point should have moved
+      ;; Make sure the result is what we expect
+;;      (setq actual-split (buffer-substring-no-properties (point-min) (point-max)))
+;;      (should (string= shu-test-cpp-general-expected-split1 actual-split))
+;;      (goto-char 20)
+;;      ;; This should restore it to its original state
+;;      (shu-cunsplit)
+;;      (setq actual-unsplit (buffer-substring-no-properties 1 (1+ (length shu-test-cpp-general-base-string))))
+;;      (should (string= shu-test-cpp-general-base-string actual-unsplit))
+      )
+))
+
+
+
+;;
+;;  shu-test-shu-csplit-2
+;;
+(ert-deftest shu-test-shu-csplit-2 ()
+  (let (
+        (actual-split)
+        (actual-unsplit)
+        (base3 "\"abcdefg\"")
+        )
+      ;; Split of very short line starting in column 1
+    (setq shu-cpp-line-end 80)
+  (with-temp-buffer
+      (insert base3)
+      (goto-char (point-min))  ;; Sitting on top of open quote
+      (shu-internal-csplit)             ;; Try to split
+      (should (= 1 (point)))   ;; Nothing should have happened
+      (forward-char 2)         ;; Move 2 chars right of quote
+      (shu-internal-csplit t)             ;; Try to split again
+      (should (= 10 (point)))  ;; Point should have moved
+      ;; But nothing should have been split
+      (setq actual-split (buffer-substring-no-properties 1 10))
+      (should (string= base3 actual-split))
+      (goto-char 4)
+      (shu-cunsplit)           ;; Unsplit should do nothing either
+      (setq actual-unsplit (buffer-substring-no-properties 1 (1+ (length base3))))
+      (should (string= base3 actual-unsplit)))
+))
+
+
+
+;;
+;;  shu-test-shu-csplit-3
+;;
+(ert-deftest shu-test-shu-csplit-3 ()
+  (let (
+        (actual-split)
+        (actual-unsplit)
+        (expected-split2
+         (concat "      \"abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-={}[]|:;'<>,.?ABCDEFGHI\"\n"
+                 "      \"JKLMNOPQRSTUVWXYZ0987654321!@#$%^&*()_+-={}[]|:;'<>,.?abcdefghijklmnopqr\"\n"
+                 "      \"stuvwxyz1234567890!@#$%^&*()_+-={}[]|:;'<>,.?ABCDEFGHIJKLMNOPQ\""))
+        )
+      ;; Split of one long line with 6 blanks in front of
+    (setq shu-cpp-line-end 80)
+  (with-temp-buffer
+      (insert (concat "      " shu-test-cpp-general-base-string))
+      (goto-char (point-min))  ;; Sitting in front of string
+      (shu-internal-csplit)             ;; Try to split
+      (should (= 1 (point)))   ;; Nothing should have happened
+      (forward-char 10)        ;; Move inside the string
+      (shu-internal-csplit t)           ;; Now try to split
+      (should (= 233 (point))) ;; Point should be here
+      (setq actual-split (buffer-substring-no-properties 1 233))
+      (should (string= expected-split2 actual-split))
+      (goto-char 30)           ;; Go inside of top string
+      (shu-cunsplit)           ;; Unsplit should restore to the original form
+      (setq actual-unsplit (buffer-substring-no-properties 7 (+ 7 (length shu-test-cpp-general-base-string))))
+      (should (string= shu-test-cpp-general-base-string actual-unsplit)))
+))
+
+
+
+;;
+;;  shu-test-shu-csplit-4
+;;
+(ert-deftest shu-test-shu-csplit-4 ()
+  (let (
+        (gb (get-buffer-create "**foo**"))
+        (actual-split)
+        (actual-unsplit)
+        (base2
+         (concat
+          "\"\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t"
+          "\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t"
+          "\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t"
+          "\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\""))
+        ;;       12345678901234567890123456789012345678901234567890123456789012345678901234567890
+        ;;                1         2         3         4         5         6         7         8
+        (expected-split3
+         (concat
+          "      \"\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\"\n"
+          "      \"\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\"\n"
+          "      \"\\t\\t\\t\\t\""))
+        )
+
+    ;; Split a long string full of tab characters
+    ;; Make sure there is no split between a "\" and a "t" in the "\t" sequence
+    (setq shu-cpp-line-end 80)
+    (with-temp-buffer
+      (insert (concat "      " base2))
+      (goto-char (point-min))  ;; Sitting in front of string
+      (shu-internal-csplit)             ;; Try to split
+      (should (= 1 (point)))   ;; Nothing should have happened
+      (forward-char 10)        ;; Move inside the string
+      (shu-internal-csplit t)           ;; Now try to split
+      (should (= 179 (point))) ;; Point should be here
+      (setq actual-split (buffer-substring-no-properties 1 179))
+      (should (string= expected-split3 actual-split))
+      (princ (concat "\n\nactual-split:\n" actual-split) gb)
+      (princ (concat "\n\nexpected-split3:\n" expected-split3) gb)
+      (goto-char 30)           ;; Go inside of top string
+      (shu-cunsplit)           ;; Unsplit should restore to the original form
+      (setq actual-unsplit (buffer-substring-no-properties 7 (+ 7 (length base2))))
+      (should (string= base2 actual-unsplit)))
+    ))
+
+
+
+;;
+;;  shu-test-shu-csplit-5
+;;
+(ert-deftest shu-test-shu-csplit-5 ()
+  (let (
+        (actual-split)
+        (actual-unsplit)
+        (base2
+         (concat
+          "\"\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t"
+          "\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t"
+          "\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t"
+          "\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\""))
+        (expected-split4
+         (concat
+          "       \"\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\\"\n"
+          "       \"t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\\t\"\n"
+          "       \"\\t\\t\\t\\t\\t\""))
+         )
+      ;; Split a long string full of tab characters shifted right one from
+  ;  previous test.  Put the "\" and "t" on different boundaries
+  ;; Make sure there is no split between a "\" and a "t" in the "\t" sequence
+  (with-temp-buffer
+    (insert (concat "       " base2))
+    (goto-char (point-min))  ;; Sitting in front of string
+    (shu-internal-csplit)             ;; Try to split
+    (should (= 1 (point)))   ;; Nothing should have happened
+    (forward-char 10)        ;; Move inside the string
+    (shu-internal-csplit t)           ;; Now try to split
+    (should (= 182 (point))) ;; Point should be here
+    (setq actual-split (buffer-substring-no-properties 1 182))
+    (should (string= expected-split4 actual-split))
+    (goto-char 30)           ;; Go inside of top string
+    (shu-cunsplit)           ;; Unsplit should restore to the original form
+    (setq actual-unsplit (buffer-substring-no-properties 8 (+ 8 (length base2))))
+    (should (string= base2 actual-unsplit)))
+  ))
 
 
 ;;; shu-new2.el ends here
