e17900a1b6e45de4303d3fa28f7cd6acaf7fa6e2
Stewart Palmer <stewart@stewartpalmer.com>
2020-05-23 08:14:07 -0400

    Experimental code for attributes

--- exp/shu-attributes.el
+++ exp/shu-attributes.el
@@ -149,6 +149,73 @@
 
 
 ;;
+;;  shu-cpp-make-nullable
+;;
+(defun shu-cpp-make-nullable (data-type)
+  "Render a data type nullable"
+    (concat "bdlb::NullableValue<" data-type ">")
+    )
+
+
+
+
+
+;;
+;;  zzx
+;;
+(defun zzx (start end)
+  "Doc string."
+  (interactive "r")
+  (let (
+        (gb (get-buffer-create "**boo**"))
+        (sline (shu-the-line-at start))
+        (eline (shu-the-line-at end))
+        (line-diff 0)
+        (eol)
+        (line)
+        (x)
+        (data-type)
+        (full-data-type)
+        (name)
+        (comment)
+        (nullable)
+
+        )
+    (while (and (<= (shu-current-line) eline) (= line-diff 0)) ; there are more lines
+      (setq eol (line-end-position))
+      (when (> eol (point))
+        (setq line (shu-trim (buffer-substring-no-properties (point) eol)))
+        (princ (concat "\n\nline: [" line "]\n") gb)
+        (when (> (length line) 1)
+        (if (string= (substring line 0 2) "//")
+            (progn
+              (setq comment (shu-trim (substring line 2)))
+              (princ (concat "comment: [" comment "]\n") gb)
+              )
+          (setq x (split-string line nil t))
+          (setq data-type (car x))
+          (setq x (cdr x))
+          (setq name (car x))
+          (setq nullable nil)
+          (setq full-data-type data-type)
+          (when (cdr x)
+            (setq nullable t)
+            (setq full-data-type (shu-cpp-make-nullable data-type))
+            )
+          (princ (concat "name: [" name "], type: [" data-type "], nullable: " (shu-bool-to-string nullable)
+                         ", full-type: [" full-data-type "]\n") gb)
+          )
+        )
+        )
+      (setq line-diff (forward-line 1))
+      )
+    (setq line-diff (forward-line 1))
+
+    ))
+
+
+
+;;
 ;;  shu-test-shu-cpp-make-attr-info-1
 ;;
 (ert-deftest shu-test-shu-cpp-make-attr-info-1 ()
