568e8de2a29fb29146135a97e59c7d36f5addda4
Stewart Palmer <stewart@stewartpalmer.com>
2021-10-26 11:29:57 -0400

    More include file types

--- lisp/shu-base.el
+++ lisp/shu-base.el
@@ -4,7 +4,7 @@
 ;;
 ;; Package: shu-base
 ;; Author: Stewart L. Palmer <stewart@stewartpalmer.com>
-;; Version: 1.6.97
+;; Version: 1.6.98
 ;; Homepage: https://github.com/codesinger/shu.git
 ;;
 ;; This file is NOT part of GNU Emacs.
@@ -35,7 +35,7 @@
 
 (provide 'shu-base)
 
-(defconst shu-version "1.6.97"
+(defconst shu-version "1.6.98"
   "The version number of the Shu elisp package.")
 
 (defconst shu-date "2019 Nov 18"
